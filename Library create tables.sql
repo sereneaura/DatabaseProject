96a00119f641372b3852cbe41024c33a5fbc0586
sereneaura <paulabigail3@gmail.com>
2022-05-25 13:42:35 +0100

    library cteate tables file edited

--- Library create tables.sql
+++ Library create tables.sql
@@ -1,37 +1,43 @@
--- create database library; --
+create database library; 
 
--- create table BOOK(
--- bookID int Not Null,
--- bookName varchar(45) Not Null,
--- bookAuthor varchar(45) Not Null,
--- constraint BOOK_pk	Primary Key(bookID)
--- );
+create table BOOK(
+bookID int Not Null,
+bookName varchar(45) Not Null,
+bookAuthor varchar(45) Not Null,
+constraint BOOK_pk	Primary Key(bookID)
+);
 
 create table borrowed_by(
 borrowID int Not Null,
-userID int Not Null,
-staffID int Not Null,
-bookID int Not Null,
 dateCollected date Not Null,
 deadline varchar(45),
-constraint borrowed_by_pk	Primary Key(borrowID)
+userID int Not Null, 
+staffID int Not Null,
+bookID int Not Null, 
+constraint borrowed_by_pk	Primary Key(borrowID),
+constraint borrowed_by_user_FK		foreign key(userID)
+				references			USER(userID),
+constraint borrowed_by_staff_FK		foreign key(staffID)
+				references			STAFF(staffID),
+constraint borrowed_by_book_FK		foreign key(bookID)
+				references			BOOK(bookID)
 );
 
--- create table STAFF(
--- staffID int Not Null,
--- FullName varchar(45) Not Null,
--- Emailaddress varchar(45) Not Null,
--- phoneNumber int Not Null,
--- address varchar(45) Not Null,
--- job varchar(45), 
--- constraint STAFF_pk	Primary Key(staffID)
--- );
+create table STAFF(
+staffID int Not Null,
+FullName varchar(45) Not Null,
+phoneNumber varchar(45) Not Null,
+Emailaddress varchar(45) Not Null,
+address varchar(45) Not Null,
+job varchar(45), 
+constraint STAFF_pk	Primary Key(staffID)
+);
 
--- create table USER (
--- 	userID		INT		NOT NULL,
--- 	fullName		VARCHAR(30)		NOT NULL,
--- 	gender		VARCHAR(10)		NOT NULL,
---     address		VARCHAR(60)		NOT NULL,
---     phoneNumber		INT		NOT NULL,
---     constraint		USER_pk		Primary Key(userID)
--- );
+create table USER (
+userID		INT		NOT NULL,
+fullName		VARCHAR(30)		NOT NULL,
+gender		VARCHAR(10)		NOT NULL,
+address		VARCHAR(60)		NOT NULL,
+phoneNumber		INT		NOT NULL,
+constraint		USER_pk		Primary Key(userID)
+);
